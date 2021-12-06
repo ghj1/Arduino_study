f4cc052fe243117136a7e1f2e728bd0f2ff452de
ghj1 <ghj12222@gmail.com>
2021-12-06 16:07:00 +0900

    cm

--- arduinofrompython.py
+++ arduinofrompython.py
@@ -6,9 +6,6 @@ while True:
     number = input("Enter a number : ")  # serial 모니터의 입력하는 부분과 같음 
     opencr.write(bytes(number, 'utf-8'))
     value = opencr.readline()   #readline이 여러값을 받을 수 있다. 
-    if value >= 20:
-        print('result1: ', value-1)
-    elif value <= 20:
-        print('result2: ', value+1)
+    
     
     # print('Result : ', value)
